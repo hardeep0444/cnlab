c8eb75e2a142ba8782d938d8d00267e4ec1a18b4
hardeep0444 <45468267+hardeep0444@users.noreply.github.com>
2021-03-06 02:02:29 +0530

    Update 3.tcl

--- 3.tcl
+++ 3.tcl
@@ -1,11 +1,12 @@
 set ns [new Simulator]
 
-set tf [open ex4.tr w]
+set tf [open 03.tr w]
 $ns trace-all $tf
 
-set nf [open ex4.nam w]
+set nf [open 03.nam w]
 $ns namtrace-all $nf
-set cwind [open win4.tr w]
+
+set cwind [open win3.tr w]
 
 $ns color 1 Blue 
 $ns color 2 Red
@@ -19,55 +20,45 @@ set n3 [$ns node]
 set n4 [$ns node]
 set n5 [$ns node]
 
-$ns duplex-link $n0 $n1 0.3Mb 10ms DropTail
-$ns duplex-link $n1 $n2 0.3Mb 10ms DropTail
-$ns duplex-link $n2 $n3 0.3Mb 10ms DropTail
-$ns duplex-link $n1 $n4 0.3Mb 10ms DropTail
-$ns duplex-link $n3 $n5 0.5Mb 10ms DropTail
-$ns duplex-link $n4 $n5 0.5Mb 10ms DropTail
+$ns duplex-link $n0 $n1 1Mb 10ms DropTail
+$ns duplex-link $n1 $n4 1Mb 10ms DropTail
+$ns duplex-link $n4 $n5 1Mb 10ms DropTail
+$ns duplex-link $n5 $n3 1Mb 10ms DropTail
+$ns duplex-link $n3 $n2 1Mb 10ms DropTail
+$ns duplex-link $n2 $n0 1Mb 10ms DropTail
 
-$ns duplex-link-op $n0 $n1 orient right
-$ns duplex-link-op $n1 $n2 orient right
-$ns duplex-link-op $n2 $n3 orient up
-$ns duplex-link-op $n1 $n4 orient up-left
-$ns duplex-link-op $n3 $n5 orient up-left
-$ns duplex-link-op $n4 $n5 orient right-up
-
-
-set tcp [new Agent/TCP]
-$ns attach-agent $n0 $tcp
-set sink [new Agent/TCPSink]
-$ns attach-agent $n5 $sink
-$ns connect $tcp $sink
-$tcp set fid_ 1
-
-set ftp [new Application/FTP]
-$ftp attach-agent $tcp
+set tcp0 [new Agent/TCP]
+$ns attach-agent $n0 $tcp0
+set sink0 [new Agent/TCPSink]
+$ns attach-agent $n4 $sink0
+$ns connect $tcp0 $sink0
+$tcp0 set fid_ 1
+set ftp0 [new Application/FTP]
+$ftp0 attach-agent $tcp0
 
 $ns rtmodel-at 1.0 down $n1 $n4
 $ns rtmodel-at 3.0 up $n1 $n4
 
-$ns at 0.1 "$ftp start"
-
-$ns at 12.0 "finish"
+$ns at 0.1 "$ftp0 start"
+$ns at 10.0 "finish"
 
 proc plotWindow {tcpSource file} {
-global ns
-set time 0.01
-set now [$ns now]
-set cwnd [$tcpSource set cwnd_]
-puts $file "$now $cwnd"
-$ns at [expr $now+$time] "plotWindow $tcpSource $file" }
-$ns at 1.0 "plotWindow $tcp $cwind"
+    global ns
+    set time 0.01
+    set now [$ns now]
+    set cwnd [$tcpSource set cwnd_]
+    puts $file "$now $cwnd"
+    $ns at [expr $now+$time] "plotWindow $tcpSource $file"
+}
+$ns at 1.0 "plotWindow $tcp0 $cwind"
 
 proc finish {} {
 	global ns tf nf cwind
 	$ns flush-trace
 	close $tf
 	close $nf
-	exec nam ex4.nam &
-        exec xgraph win4.tr & 
+	exec nam 03.nam &
+    	exec xgraph win3.tr & 
 	exit 0
 }
-
 $ns run
