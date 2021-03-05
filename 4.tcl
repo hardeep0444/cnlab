b6b7376355934889548d2209a518bdde39960771
hardeep0444 <45468267+hardeep0444@users.noreply.github.com>
2021-03-06 02:03:06 +0530

    Update 4.tcl

--- 4.tcl
+++ 4.tcl
@@ -1,60 +1,38 @@
-#Create a ns simulator
 set ns [new Simulator]
 
-#Open the NS trace file
-set tracefile [open ex5.tr w]
-$ns trace-all $tracefile
+set tf [open 04.tr w]
+$ns trace-all $tf
 
-#Open the NAM trace file
-set namfile [open ex5.nam w]
-$ns namtrace-all $namfile
+set nf [open 04.nam w]
+$ns namtrace-all $nf
 
-#Create 2 nodes
-set s [$ns node]
-set c [$ns node]
+set n0 [$ns node]
+set n1 [$ns node]
 
-$ns color 1 Blue
+$ns duplex-link $n0 $n1 10Mb 22ms DropTail
 
-#Create labels for nodes
-$s label "Server"
-$c label "Client"
-
-#Create links between nodes
-$ns duplex-link $s $c 10Mb 22ms DropTail
-
-#Give node position (for NAM)
-$ns duplex-link-op $s $c orient right
-
-		
-#Setup a TCP connection for node s(server)
 set tcp0 [new Agent/TCP]
-$ns attach-agent $s $tcp0
+$ns attach-agent $n0 $tcp0
 $tcp0 set packetSize_ 1500
-
-#Setup a TCPSink connection for node c(client)
 set sink0 [new Agent/TCPSink]
-$ns attach-agent $c $sink0
-
+$ns attach-agent $n1 $sink0
 $ns connect $tcp0 $sink0
-
-#Setup a FTP Application over TCP connection
 set ftp0 [new Application/FTP]
 $ftp0 attach-agent $tcp0
+$ns at 0.01 "$ftp0 start"
 
-$tcp0 set fid_ 1
+$ns at 15.1 "finish"
 
 proc finish { } {
-	global ns tracefile namfile 
+	global ns tf nf 
 	$ns flush-trace
-	close $tracefile
-    	close $namfile
-    	exec nam ex5.nam &
-	exec awk -f ex5transfer.awk ex5.tr &
-	exec awk -f ex5convert.awk  ex5.tr > convert.tr &
-	exec xgraph convert.tr -geometry 800*400 -t "bytes_received_at_client" -x "time_in_secs" -y "bytes_in_bps"  &
-		}
+	close $tf
+	close $nf
+         exec nam 04.nam &
+	#exec awk -f 04t.awk 04.tr &
+	#exec awk -f 04c.awk 04.tr > convert.tr &
+	#exec xgraph convert.tr -geometry 800*400 -t "bytes_received_at_client" -x "time_in_secs" -y "bytes_in_bps"  &
+	exit 0
+}
 
-$ns at 0.01 "$ftp0 start"
-$ns at 15.0 "$ftp0 stop"
-$ns at 15.1 "finish"
 $ns run
